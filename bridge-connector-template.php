e2298b25b27f64d84c8ddabdc77bca3810cafab6
Ole Poetter <48469242+olpo24@users.noreply.github.com>
2026-01-02 11:42:54 +0100

    Update bridge-connector-template.php

--- bridge-connector-template.php
+++ bridge-connector-template.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: WP Bridge Connector
-Description: Connector für WP Maintenance Monitor.
-Version: 1.0.0
+Description: Connector für WP Maintenance Monitor Dashboard.
+Version: 1.0.1
 */
 
 $api_key = 'YOUR_API_KEY_HERE';
@@ -10,33 +10,36 @@ $api_key = 'YOUR_API_KEY_HERE';
 add_action('rest_api_init', function () {
     register_rest_route('bridge/v1', '/status', [
         'methods' => 'GET',
-        'callback' => function() {
-            return [
-                'version' => get_bloginfo('version'),
-                'updates' => [
-                    'counts' => ['plugins' => count(get_plugin_updates()), 'themes' => count(get_theme_updates())],
-                    'plugin_names' => array_keys(get_plugin_updates())
-                ]
-            ];
-        },
+        'callback' => 'wpbc_get_status',
         'permission_callback' => 'wpbc_check'
     ]);
-
     register_rest_route('bridge/v1', '/get-login-url', [
         'methods' => 'GET',
-        'callback' => function() {
-            $admin = get_users(['role' => 'administrator'])[0];
-            $token = bin2hex(random_bytes(20));
-            update_option('wpbc_sso_' . $token, $admin->ID, false);
-            return ['success' => true, 'login_url' => add_query_arg('bridge_sso', $token, admin_url())];
-        },
+        'callback' => 'wpbc_get_login',
         'permission_callback' => 'wpbc_check'
     ]);
 });
 
+function wpbc_get_status() {
+    return [
+        'version' => get_bloginfo('version'),
+        'updates' => [
+            'counts' => ['plugins' => count(get_plugin_updates()), 'themes' => count(get_theme_updates())],
+            'plugin_names' => array_keys(get_plugin_updates())
+        ]
+    ];
+}
+
+function wpbc_get_login() {
+    $admins = get_users(['role' => 'administrator', 'number' => 1]);
+    $token = bin2hex(random_bytes(20));
+    update_option('wpbc_sso_' . $token, $admins[0]->ID, false);
+    return ['success' => true, 'login_url' => add_query_arg('bridge_sso', $token, admin_url())];
+}
+
 add_action('init', function() {
     if(isset($_GET['bridge_sso'])) {
-        $token = $_GET['bridge_sso'];
+        $token = sanitize_text_field($_GET['bridge_sso']);
         $user_id = get_option('wpbc_sso_' . $token);
         if($user_id) {
             wp_set_auth_cookie($user_id);

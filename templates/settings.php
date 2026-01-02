21fe627864d9aa382dbeb7371fbaaf61b952d834
Ole Poetter <48469242+olpo24@users.noreply.github.com>
2026-01-02 11:44:02 +0100

    Update settings.php

--- templates/settings.php
+++ templates/settings.php
@@ -2,24 +2,13 @@
 <div class="wrap">
     <h1>Einstellungen</h1>
     <?php if (isset($_GET['wpmm_added'])): ?>
-        <div class="notice notice-success is-dismissible" style="padding:15px;">
-            <p><strong>✔ Seite registriert!</strong></p>
-            <p>1. Lade das Bridge-Plugin herunter: 
-               <a href="admin.php?action=download_bridge&api_key=<?= esc_attr($_GET['api_key']) ?>" class="button button-primary">Bridge (.php) laden</a>
-            </p>
-            <p>2. Installiere es auf der Zielseite unter <code>/wp-content/plugins/</code>.</p>
+        <div class="notice notice-success is-dismissible">
+            <p>Seite registriert! <a href="admin.php?action=download_bridge&api_key=<?= esc_attr($_GET['api_key']) ?>" class="button button-small">Bridge laden</a></p>
         </div>
     <?php endif; ?>
-
-    <div class="postbox" style="margin-top:20px;">
-        <div class="inside">
-            <form id="add-site-form">
-                <table class="form-table">
-                    <tr><th>Anzeigename</th><td><input type="text" id="site-name" class="regular-text" required></td></tr>
-                    <tr><th>Website URL</th><td><input type="url" id="site-url" class="regular-text" required></td></tr>
-                </table>
-                <input type="submit" class="button button-primary" value="Seite registrieren">
-            </form>
-        </div>
-    </div>
+    <form id="add-site-form" class="postbox" style="padding:20px;">
+        <p><label>Name</label><br><input type="text" id="site-name" class="regular-text" required></p>
+        <p><label>URL</label><br><input type="url" id="site-url" class="regular-text" required></p>
+        <input type="submit" class="button button-primary" value="Registrieren">
+    </form>
 </div>
